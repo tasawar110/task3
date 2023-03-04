d280767921332c140ce4a6763d6b8c9ff9b60ed7
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-04 03:07:22 -0800

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,9 @@
+#task1
+Formercreep=('Ali','Asad','Farah','Shahmeer','subhan','salar','hafsa','sana','Hora','Ahad','Saif','Maryam','Irfan','Furqan','Mehrooz')
+print(Formercreep)
+#task2
+flavors=['orange','pineapple','grapes','tutyfruty','pepsi','pakola','reditrish']
+print(flavors)
+##task3
+products=['ventilatos','CTscan','Defibrillator','Patient Monitos','Syringes','MRI']
+print(products)
